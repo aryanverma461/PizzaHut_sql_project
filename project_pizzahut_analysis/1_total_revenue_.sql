6ff38c4f28237dd7d89f8b78683ea42b77ec3dbd
aryanverma461 <vermaaryan461@gmail.com>
2024-08-10 15:39:03 +0530

    push change

--- project_pizzahut_analysis/1_total_revenue_.sql
+++ project_pizzahut_analysis/1_total_revenue_.sql
@@ -8,6 +8,15 @@ INNER JOIN pizzas
     ON 
     order_details.pizza_id = pizzas.pizza_id
 
+/* RESULT:
+[
+  {
+    "sum": "817860.05"
+  }
+]
+*/
+
+
 /*Calculated the total revenue generated from the pizza
       sales by each order_quantity  */
 
@@ -23,3 +32,27 @@ INNER JOIN pizzas
 GROUP BY order_details.quantity
 ORDER BY
         sum DESC
+        
+/*Insights generated:
+The data indicates that customers are more likely to purchase single pizzas rather 
+than multiple pizzas in one order. This could be due to individual dining 
+preferences or smaller group sizes.
+[
+  {
+    "quantity": 1,
+    "sum": "786457.10"
+  },
+  {
+    "quantity": 2,
+    "sum": "30209.20"
+  },
+  {
+    "quantity": 3,
+    "sum": "1014.75"
+  },
+  {
+    "quantity": 4,
+    "sum": "179.00"
+  }
+]
+*/
